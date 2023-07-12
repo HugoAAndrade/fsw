d2d937286006b05c64995756e786f106ebf3565a
Hugo Andrade <hugoalvesandrade@hotmail.com>
2023-07-11 21:07:33 -0300

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import Trips from "@/app/about/components/Trips";
+
+export const metadata = {
+  title: "About",
+};
+
+const About = () => {
+  return (
+    <div>
+      <Trips />
+    </div>
+  );
+};
+
+export default About;
